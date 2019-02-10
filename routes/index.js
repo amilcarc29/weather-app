b2657492800f9a091d0161176ed6abeea699289f
Amilcar Cattaneo <amilcar.cattaneo29@gmail.com>
2019-02-10 00:53:02 -0300

    Express call to get the location weather.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,31 @@
 const router = require("express").Router();
 const path = require("path");
+const fetch = require("node-fetch");
+
+const apiKey = "2e53218b7e31aa63d6596df481834fab";
 
 router.use(function(req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-module.exports = router;
+module.exports = app => {
+  let city;
+  app.post("/search-location", (req, res) => {
+    city = req.body.city;
+    res.redirect("/");
+  });
+
+  app.get("/search-location-weather", (req, res) => {
+    if (city) {
+      fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+      )
+        .then(res => res.json())
+        .then(data => res.send({ data }))
+        .catch(err => {
+          res.redirect("/error");
+        });
+      city = "";
+    }
+  });
+};
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const routes = require("./routes");
 const app = express();
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("client/build"));
-app.use(routes);
+
+require("./routes")(app);
 
 app.listen(5000, function() {
   console.log("Weather app listening on port 5000");
